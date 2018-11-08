bf9929c54ed7c204cc7dd68c9c13e1f4afff8583
lai_ye <lai_ye@hunliji.com>
2018-11-08 14:37:12 +0800

    run_method

--- Common/run_method.py
+++ Common/run_method.py
@@ -1,22 +1,26 @@
 import requests
 class RunMethod:
     def post_res(self,url,data=None,header=None):
-        res=requests.get(url=url,data=data,headers=header)
+        res=requests.post(url=url,data=data,headers=header)
         return res
-    def get_pos(self,url,data=None,header=None):
-        res = requests.post(url=url, data=data, headers=header)
+    def get_res(self,url,data=None,header=None):
+        res = requests.get(url=url, data=data, headers=header)
         return res
     def run_res(self,url,method,data=None,header=None):
         if method=='post':
-            res = self.post_res(url,data,header)
+            res = self.post_res(url, data, header)
         else:
-            res = self.get_pos(url, data, header)
+            res = self.get_res(url, data, header)
         return res
 
 if __name__ == '__main__':
-    url=''
-    data={}
-    header={}
+    url='http://dev.hunliji.com/p/wedding/index.php/Admin/APILiveChannelReport/live_report_protocol'
+    data={
+        "live_type":1
+    }
+    header={
+        "Cookie":"HUNLIJI_MERCHANT_AUTH=362165%2C1539159071%2C6873f52e2c4da8adcee26e11be4df93d;"
+    }
     res=RunMethod()
-    print(res.get_pos(url,data,header))
-    print(res.post_res(url, data, header))
+    print(res.run_res(url=url,method='get',data=data,header=header).json())
+
