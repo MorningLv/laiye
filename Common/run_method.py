199084673303bd9292db42a2287d0398a8288a1d
lai_ye <lai_ye@hunliji.com>
2018-11-08 14:38:59 +0800

    run_method

--- Common/run_method.py
+++ Common/run_method.py
@@ -18,9 +18,6 @@ if __name__ == '__main__':
     data={
         "live_type":1
     }
-    header={
-        "Cookie":"HUNLIJI_MERCHANT_AUTH=362165%2C1539159071%2C6873f52e2c4da8adcee26e11be4df93d;"
-    }
     res=RunMethod()
     print(res.run_res(url=url,method='get',data=data,header=header).json())
 
